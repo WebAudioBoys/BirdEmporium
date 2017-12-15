d6b3169c41c8873cf116cc4a1c3a3c708c30eae0
Brad Gowland <brg301@nyu.edu>
2017-12-15 18:33:13 -0500

    pretty printing and more trees in forest

--- classify.py
+++ classify.py
@@ -10,8 +10,14 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
+mfccs = np.load('../SavedVariables/all_mfccs.npy')
+labels = np.load('../SavedVariables/BirdLabels32.npy') 
+
+print("Splitting testing and training data.")
 X_train, X_test, y_train, y_test = train_test_split(mfccs.transpose(), labels, test_size=0.20, random_state=42)
-forest = RandomForestClassifier(n_estimators=100, random_state=0)
+print("Preparing random forest.")
+forest = RandomForestClassifier(n_estimators=200, random_state=0, max_features=4)
+print("Classifying.")
 forest.fit(X_train, y_train)
 
 print("Accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
--- createFeatures.py
+++ createFeatures.py
@@ -16,9 +16,6 @@ finchFrames = np.load('../SavedVariables/finchTruncatedFrames32.npy')
 robinFrames = np.load('../SavedVariables/robinTruncatedFrames32.npy')
 labels = np.load('../SavedVariables/BirdLabels32.npy')
 
-allBirds = [orioleFrames, cardinalFrames, chickadeeFrames,finchFrames,robinFrames]
-
-
 min_freq = 500
 max_freq = 10000
 num_mel_filts = 26
@@ -27,25 +24,27 @@ win_size = 512
 hop_size = 256
 fs = 32000
 
-
-
+print("Oriole MFCCs calculating.")
 oriole_mfccs, mfcc_fs = compute_mfccs_frames(orioleFrames,fs,
 hop_size,min_freq,max_freq,num_mel_filts, n_dct)
 
+print("Cardinal MFCCs calculating.")
 cardinal_mfccs, mfcc_fs = compute_mfccs_frames(cardinalFrames,fs,
 hop_size,min_freq,max_freq,num_mel_filts, n_dct)
 
+print("Chickadee MFCCs calculating.")
 chickadee_mfccs, mfcc_fs = compute_mfccs_frames(chickadeeFrames,fs,
 hop_size,min_freq,max_freq,num_mel_filts, n_dct)
 
+print("Goldfinch MFCCs calculating.")
 finch_mfccs, mfcc_fs = compute_mfccs_frames(finchFrames,fs,
 hop_size,min_freq,max_freq,num_mel_filts, n_dct)
 
+print("Robin MFCCs calculating.")
 robin_mfccs, mfcc_fs = compute_mfccs_frames(robinFrames,fs,
 hop_size,min_freq,max_freq,num_mel_filts, n_dct)
 
-
-
+print("Saving results.")
 all_mfccs = np.hstack((oriole_mfccs, cardinal_mfccs, chickadee_mfccs, finch_mfccs, robin_mfccs))
 np.save('../SavedVariables/all_mfccs', all_mfccs)
 #num_frames_per_class = np.bincount(labels.astype(int))
