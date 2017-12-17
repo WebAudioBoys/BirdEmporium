eac2e1f41d32386d454d3f2bc51d768a8c9de7ce
Brad Gowland <brg301@nyu.edu>
2017-12-17 17:51:10 -0500

    stats
    stats and pretty printing the results report

--- classify.py
+++ classify.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 mfccs = np.load('../SavedVariables/all_mfccs.npy')
 labels = np.load('../SavedVariables/BirdLabels32.npy') 
+birds = ["Oriole", "Cardinal", "Chickadee", "Goldfinch", "Robin"]
 
 print("Splitting testing and training data.")
 X_train, X_test, y_train, y_test = train_test_split(mfccs.transpose(), labels, test_size=0.20, random_state=42)
@@ -19,14 +20,38 @@ forest = RandomForestClassifier(n_estimators=100, random_state=0, max_features=4
 print("Classifying.")
 forest.fit(X_train, y_train)
 
-print("Accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
-print("Accuracy on test set: {:.3f}".format(forest.score(X_test, y_test)))
+print("--------------------")
+print("Results report")
+print("--------------------")
 
-# TODO: clean this up and make some pretty printing
-print("Calculating per class accuracies:")
+print("Overall accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
+print("Overall accuracy on test set: {:.3f}".format(forest.score(X_test, y_test)))
+print("--------------------")
+
+# precision, recall, f measure stats
 p_labels = forest.predict(X_test)
+
+class_precision = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+class_recall = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+class_f_measure = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+
 for i in range(0,5):
-    class_accuracy = len(p_labels[(p_labels==y_test) & (p_labels==i)])/len(p_labels[p_labels==i])
-    print(class_accuracy)
-    
-# TODO: precision and recall stats
+    c = len(p_labels[(p_labels==y_test) & (p_labels==i)])
+    f_p = len(p_labels[(p_labels!=y_test) & (p_labels==i)])
+    f_n = len(p_labels[(p_labels!=y_test) & (y_test==i)])
+    class_precision[i] = c/(c + f_p)
+    class_recall[i] = c/(c + f_n)
+    class_f_measure[i] = (2 * class_precision[i] * class_recall[i]) / (class_precision[i] + class_recall[i])
+    print("%s results:" % birds[i])
+    print("Precision: %f" % class_precision[i])
+    print("Recall: %f" % class_recall[i])
+    print("F Measure: %f" % class_f_measure[i])
+    print("--------------------")
+
+avg_precision = np.average(class_precision)
+avg_recall = np.average(class_recall)
+avg_f_measure = np.average(class_f_measure)
+
+print("Average precision: %f" % avg_precision)
+print("Average recall: %f" % avg_recall)
+print("Average F measure: %f" % avg_f_measure)
